4ec03196d9dfb3da11855e677869329ed03a791d
Bernd Heesen <60698023+bheesen@users.noreply.github.com>
2024-04-03 14:34:05 +0200

    Add files via upload

---
+++ Python/4-DrittesPythonProgramm.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 14:06:52 2024
+
+@author: bernd
+"""
+
+#%% Paket ohne Abkürzung
+import datetime
+# Aktuelle Zeit und Datum
+now1 = datetime.datetime.now()
+print(now1)
+#%% Paket mit Abkürzung
+import datetime as dt
+# Aktuelle Zeit und Datum
+now2 = dt.datetime.now()
+print(now2)
---
+++ Python/5-ViertesPythonProgramm-from-RStudio.py
@@ -0,0 +1,3 @@
+zahlenliste = list(range(1,30,3))
+print(zahlenliste)
+
